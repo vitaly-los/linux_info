d62662438cf678c0545884767cb9debddf1dad3a
Los Vitaly <38357863+vitaly-los@users.noreply.github.com>
2020-08-16 12:32:49 +0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -8,3 +8,14 @@ then
 echo $FILE found
 fi
 
+# Flags for -eq equal, -lt less, -gt gtrater
+if [[ $VAL -lt $MIN ]]
+then
+echo "value is less than min"
+fi
+
+# inside () no need for $ sign, only for $1, $#, $0
+if (( VAL < 10 ))
+then
+echo "value $VAL is less than ten"
+fi
