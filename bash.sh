abad571f4901358b50f167e56dcd1c934b8f8858
Los Vitaly <38357863+vitaly-los@users.noreply.github.com>
2020-08-16 12:19:19 +0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,3 +2,9 @@
 
 # 0 or false meant success or true in bash
 
+# check if file exists. Flags for -d directory, -e file, -r readable, -W writable, -X executuble
+if [[ -e $FILE ]]
+then
+echo $FILE found
+fi
+
